071296e4b432fac8a0074de539895d73bf82638a
Christopher Dunn <cdunn@pacificbiosciences.com>
2021-10-15 08:33:01 -0700

    better indent

--- tests/t_get_ref_alt.nim
+++ tests/t_get_ref_alt.nim
@@ -1,6 +1,9 @@
+# vim: sts=2:ts=2:sw=2:et:tw=0
 import nibsv
 import unittest
 
 suite "nibsv":
-    test "foo":
-        check parse_sv_allele("hi") == 1
+  test "foo":
+    let
+      s = "T[chr1_KI270709v1_random:461["
+    check parse_sv_allele(s) == 1
